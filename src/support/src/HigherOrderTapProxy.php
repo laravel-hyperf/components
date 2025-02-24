6a625d6dc0f2d565ecd86f852d0bf8f2217a8614
Albert Chen <albert.cht@gmail.com>
2025-02-24 17:49:13 +0800

    feat: add HigherOrderTapProxy

---
+++ src/support/src/HigherOrderTapProxy.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaravelHyperf\Support;
+
+use Hyperf\Tappable\HigherOrderTapProxy as HyperfHigherOrderTapProxy;
+
+class HigherOrderTapProxy extends HyperfHigherOrderTapProxy
+{
+}
